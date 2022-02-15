994cab07127463fc33444a70cf0ae4417bb86e9f
TKei-git <toxaie3@gmail.com>
2022-02-15 14:26:19 +0000

    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_161608) do
+ActiveRecord::Schema.define(version: 2022_02_15_132443) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
